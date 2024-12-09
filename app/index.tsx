6fd8076e79bc9b3345e758be435530cf640b7442
AnthonyScott <anthony.scott1005@gmail.com>
2024-12-09 02:33:11 -0500

    Add form

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,73 @@
-import { Text, View } from "react-native";
+import { useState } from 'react'
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  StyleSheet,
+  TextInput
+} from "react-native";
+
+
 
 export default function Index() {
+  const [input, setInput] = useState('')
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View
+        style={[
+          styles.imgContainer,
+          {
+            flexDirection: 'row',
+          },
+        ]}>
+        <Image
+          source={
+            require('./assets/images/react-logo.png')
+          }
+        />
+        <Image
+          source={
+            require('./assets/images/partial-react-logo.png')
+          }
+          style={{ height: 100, width: 100 }}
+        />
+      </View>
+
+      <View
+        style={styles.txtContainer}
+      >
+        <Text>Check it out!</Text>
+        <TextInput
+          onChangeText={newText => setInput(newText)}
+          placeholder='What framework is this?'
+          style={styles.inputBox}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 0,
+    flex: 1,
+    flexDirection: 'column'
+  },
+  imgContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  txtContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    gap: 10,
+    padding: 12,
+  },
+  inputBox: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 3,
+  },
+})
